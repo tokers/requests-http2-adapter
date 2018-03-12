dfa71ca44af051bbc46826f008fbc3d49e0f10c3
Alex Zhang (tokers) <zchao1995@gmail.com>
2018-03-12 13:24:22 +0800

    chore: introduced requests as submodule and newed __init__.py
    Signed-Offed-By: Alex Zhang (tokers) <zchao1995@gmail.com>

---
+++ __init__.py
@@ -0,0 +1,10 @@
+ # -*- coding: utf-8 -*-
+
+"""
+HTTP/2 Adapter for Requests HTTP Library
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+:copyright: (c) 2018 by Alex Zhang(tokers).
+:license: Apache 2.0, see LICENSE for more details.
+"""
